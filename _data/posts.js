c0bce3c852834ea5a2fe943768f1ff9af510fcfc
Bryan Robinson <bryanlrobinson@gmail.com>
2024-02-09 12:37:40 -0500

    Pops the table of contents

--- _data/posts.js
+++ _data/posts.js
@@ -1,4 +1,9 @@
 const GraphQLClient = require('graphql-request').GraphQLClient
+const isHeader = require('../utils/isHeader')
+const addId = require('../utils/addId')
+
+const {astToHtmlString} = require('@graphcms/rich-text-html-renderer')
+const generateJSON = require('nested-toc-json-generator')
 
 // Get Hygraph posts for 11ty data
 const getHygraphPosts = async () => {
@@ -19,10 +24,43 @@ const getHygraphPosts = async () => {
     return response.posts
 }
 
+const renderers = {
+  h1: ({id, children}) => `<h1 id="${id}"><a href="#${id}">${children}</a></h1>`,
+  h2: ({id, children}) => `<h2 id="${id}"><a href="#${id}">${children}</a></h2>`,
+  h3: ({id, children}) => `<h3 id="${id}"><a href="#${id}">${children}</a></h3>`,
+  h4: ({id, children}) => `<h4 id="${id}"><a href="#${id}">${children}</a></h4>`,
+  h5: ({id, children}) => `<h5 id="${id}"><a href="#${id}">${children}</a></h5>`,
+  h6: ({id, children}) => `<h6 id="${id}"><a href="#${id}">${children}</a></h6>`
+}
+
+
+async function addContent(post) {
+  const content = post.content.json.children
+  const contentWithIds = content.map(addId)
+
+  const html = await astToHtmlString({content: contentWithIds, renderers: renderers})
+
+
+  const headers = contentWithIds.filter(isHeader)
+  const headerJson = headers.map(header => ({
+    ...header,
+    text: header.children[0].text
+  }))
+  console.log(headerJson)
+
+  const tocArray = await generateJSON(headerJson)
+
+  return {
+    ...post,
+    html,
+    tocArray
+  }
+
+}
 
 
 module.exports = async () => {
     const posts = await getHygraphPosts()
-
-    return posts
+    const postsWithContent = posts.map(addContent)
+    return postsWithContent
 }
