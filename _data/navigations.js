156c6793e6a3ca2c4a2b0dcc3f176a54dc04f03d
Bryan Robinson <bryanlrobinson@gmail.com>
2024-02-12 15:01:10 -0500

    Brings feature parity with next starter

---
+++ _data/navigations.js
@@ -0,0 +1,42 @@
+const GraphQLClient = require('graphql-request').GraphQLClient
+
+// Get Hygraph posts for 11ty data
+const getHygraphPosts = async () => {
+    const client = new GraphQLClient('https://api-us-west-2.hygraph.com/v2/cljhg3w940kcw01um5tov6e2a/master')
+    const response = await client.request(`
+    query MyQuery {
+            navigations {
+              id
+              link {
+                externalUrl
+                displayText
+                page {
+                  ... on Page {
+                    id
+                    slug
+                  }
+                }
+              }
+              navId
+            
+            }
+          }
+      
+`    )
+
+    const navById = {}
+    
+    response.navigations.forEach((nav) => {
+      navById[nav.navId] = nav   
+    })
+
+    return navById
+}
+
+
+
+module.exports = async () => {
+    const pages = await getHygraphPosts()
+
+    return pages
+}
--- _data/posts.js
+++ _data/posts.js
@@ -1,66 +1,99 @@
-const GraphQLClient = require('graphql-request').GraphQLClient
-const isHeader = require('../utils/isHeader')
-const addId = require('../utils/addId')
+const GraphQLClient = require("graphql-request").GraphQLClient;
+const isHeader = require("../utils/isHeader");
+const addId = require("../utils/addId");
 
-const {astToHtmlString} = require('@graphcms/rich-text-html-renderer')
-const generateJSON = require('nested-toc-json-generator')
+const { astToHtmlString } = require("@graphcms/rich-text-html-renderer");
+const generateJSON = require("nested-toc-json-generator");
 
 // Get Hygraph posts for 11ty data
 const getHygraphPosts = async () => {
-    const client = new GraphQLClient('https://api-us-west-2.hygraph.com/v2/cljhg3w940kcw01um5tov6e2a/master')
-    const response = await client.request(`
+  const client = new GraphQLClient(
+    "https://api-us-west-2.hygraph.com/v2/cljhg3w940kcw01um5tov6e2a/master"
+  );
+  const response = await client.request(`
     query MyQuery {
         posts {
           slug
           title
+          excerpt
+          createdAt
+          author {
+            name
+            title
+            picture {
+              height
+              width
+              url(transformation: {image: {resize: {fit: scale, width: 200}}})
+            }
+          }
           content {
             json
-            html
+            references {
+              ... on Asset {
+                id
+                url
+                mimeType
+                size
+                handle
+              }
+              ... on Author {
+                id
+                name
+              }
+            }
           }
         }
       }
-`    )
+`);
 
-    return response.posts
-}
+  return response.posts;
+};
 
 const renderers = {
-  h1: ({id, children}) => `<h1 id="${id}"><a href="#${id}">${children}</a></h1>`,
-  h2: ({id, children}) => `<h2 id="${id}"><a href="#${id}">${children}</a></h2>`,
-  h3: ({id, children}) => `<h3 id="${id}"><a href="#${id}">${children}</a></h3>`,
-  h4: ({id, children}) => `<h4 id="${id}"><a href="#${id}">${children}</a></h4>`,
-  h5: ({id, children}) => `<h5 id="${id}"><a href="#${id}">${children}</a></h5>`,
-  h6: ({id, children}) => `<h6 id="${id}"><a href="#${id}">${children}</a></h6>`
-}
-
+  h1: ({ id, children }) =>
+    `<h1 id="${id}"><a href="#${id}">${children}</a></h1>`,
+  h2: ({ id, children }) =>
+    `<h2 id="${id}"><a href="#${id}">${children}</a></h2>`,
+  h3: ({ id, children }) =>
+    `<h3 id="${id}"><a href="#${id}">${children}</a></h3>`,
+  h4: ({ id, children }) =>
+    `<h4 id="${id}"><a href="#${id}">${children}</a></h4>`,
+  h5: ({ id, children }) =>
+    `<h5 id="${id}"><a href="#${id}">${children}</a></h5>`,
+  h6: ({ id, children }) =>
+    `<h6 id="${id}"><a href="#${id}">${children}</a></h6>`,
+  embed: {
+    Author: ({name}) => `<div class="authorBox"><p>Author: ${name}</p></div>`,
+  }
+};
 
 async function addContent(post) {
-  const content = post.content.json.children
-  const contentWithIds = content.map(addId)
+  const content = post.content.json.children;
+  const contentWithIds = content.map(addId);
 
-  const html = await astToHtmlString({content: contentWithIds, renderers: renderers})
+  const html = await astToHtmlString({
+    content: contentWithIds,
+    renderers: renderers,
+    references: post.content.references,
+  });
 
-
-  const headers = contentWithIds.filter(isHeader)
-  const headerJson = headers.map(header => ({
+  const headers = contentWithIds.filter(isHeader);
+  const headerJson = headers.map((header) => ({
     ...header,
-    text: header.children[0].text
-  }))
-  console.log(headerJson)
+    text: header.children[0].text,
+  }));
 
-  const tocArray = await generateJSON(headerJson)
+  const tocArray = await generateJSON(headerJson);
 
   return {
     ...post,
     html,
-    tocArray
-  }
-
+    tocArray,
+  };
 }
-
 
 module.exports = async () => {
-    const posts = await getHygraphPosts()
-    const postsWithContent = posts.map(addContent)
-    return postsWithContent
-}
+  const posts = await getHygraphPosts();
+  const postsWithContent = posts.map(addContent);
+  return postsWithContent;
+};
